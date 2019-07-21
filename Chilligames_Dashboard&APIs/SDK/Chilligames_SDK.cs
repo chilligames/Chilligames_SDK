b53c970d2f67c9690c222868db0ec19bad4fae6a
Hossynkoala <42633146+Hossynkoala@users.noreply.github.com>
2019-07-21 22:15:28 +0430

    SDK->Initialize add SDK->api_Client add NameSpace->model_clinet

--- Chilligames_Dashboard&APIs/SDK/Chilligames_SDK.cs
+++ Chilligames_Dashboard&APIs/SDK/Chilligames_SDK.cs
@@ -2,47 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System;
+using Chilligames.SDK.Model_Client;
 
 
+namespace Chilligames.SDK.Model_Client
+{
+    public class Req_reg_user_Username_pass
+    {
+        public string UserName;
+        public string Password;
+    }
+
+}
 namespace Chilligames.SDK
 {
 
-
     public class Chilligames_SDK : MonoBehaviour
     {
-        public class Gate : MonoBehaviour
-        {
-            public static bool Fast_close
-            {
-                get => default;
-                set
-                {
-                }
-            }
 
-            public void OpenGate()
-            {
-                
-            }
-
-
-        }
-
-        public static class API_Client
-        {
-            public static void Get_Player_info()
-            {
-
-            }
-            
-
-
-        }
-
-        public class API_Admin
+        public static  void Initialize(string Token_user,string Token_app)
         {
 
         }
+
+
+        internal class API_Client
+        {
+
+            public static void Register_Users_with_Username_Password(Req_reg_user_Username_pass Requst_register, Action<Result_register> Result, Action<ERROR_register> ERROR)
+            {
+
+
+
+
+            }
+
+
+
+            public class Result_register
+            {
+
+            }
+            public class ERROR_register
+            {
+
+            }
+        }
+
+
+        internal class API_Admin
+        {
+
+        }
+
+
     }
 
 
